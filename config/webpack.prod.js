1d29bbe95073a2c73d8a92c8688d3c44aa072310
linmingzhe <linmingzhe@foxmail.com>
2016-09-21 15:53:42 +0800

    prod去掉sourcemap

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -46,7 +46,7 @@ module.exports = function(env) {
      * See: http://webpack.github.io/docs/configuration.html#devtool
      * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
      */
-    devtool: 'source-map',
+    //devtool: 'source-map',
 
     /**
      * Options affecting the output of the compilation.
